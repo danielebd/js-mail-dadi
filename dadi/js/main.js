3f52be7db316214526189e868ab57196c94cf382
danielebd <danielebalebalestro@gmail.com>
2023-02-24 18:47:59 +0100

    creazione html e css

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,17 +1,43 @@
 'use strict';
-const giocatore = Math.floor(Math.random() * 6) + 1;
-console.log('punteggio giocatore ' + giocatore);
-const computer = Math.floor(Math.random() * 6) + 1;
-console.log('punteggio computer ' + computer);
 
-if(giocatore < computer){
-    console.log('hai perso');
-    alert('hai perso');
-} else if (giocatore === computer){
-    console.log('pareggio');
-    alert('pareggio');
-} else{
-    console.log('hai vinto')
-    alert('hai vinto');
-}
+const button = document.getElementById('inputButton');
+let x = ('');
+
+
+button.addEventListener('click',
+
+    function () {
+        const giocatore = Math.floor(Math.random() * 6) + 1;
+        console.log('punteggio giocatore ' + giocatore);
+        const computer = Math.floor(Math.random() * 6) + 1;
+        console.log('punteggio computer ' + computer);
+
+        if (giocatore < computer) {
+            console.log('hai perso');
+            //alert('hai perso');
+            x = ('HAI PERSO, SCHIAPPA!');
+
+        } else if (giocatore === computer) {
+            console.log('pareggio');
+            //alert('pareggio');
+            x = ('PAREGGIATO, CHE NOIA');
+
+        } else {
+            console.log('hai vinto')
+            //alert('hai vinto');
+            x = ('HAI VINTOOOO!!!');
+
+        }
+
+
+        document.querySelector('.giocatore').innerHTML =`TU: ${giocatore}`;
+        document.querySelector('.computer').innerHTML =`PC: ${computer}`;
+        document.querySelector('#risultato').innerHTML =`${x}`;
+
+        const boxRisultato = document.querySelector('.box-risultato');
+        boxRisultato.classList.add("show");
+        
+    }
+    
+)
 
--- dadi/readme.md
+++ dadi/readme.md
@@ -1,3 +1,5 @@
 - creare elemento giocatore per estrazione numero da 1 a sei
 - creare elemento computer per estrazione numero da 1 a sei
-- creare condizionale che se punteggio giocatore è maggiore di quello del pc esce allert hai vinto altrimenti se minore hai perso
+- creare condizionale che se punteggio giocatore è maggiore di quello del pc esce allert hai vinto altrimenti se minore hai perso.
+
+creare un html e css esteticamente piacevole
